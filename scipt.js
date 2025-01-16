1c5ff4a3fbad30cfd97cf725a558545f5ed28213
Ngen Chanheng <ngenchanheng002@gmail.com>
2025-01-16 13:52:25 +0700

    motified_toggle

--- scipt.js
+++ scipt.js
@@ -1,14 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const toggle = document.querySelector(".menu-toggle");
-    const menuDropDown = document.querySelector(".menu-drob");
-    toggle.addEventListener("click", () => {
-        if(menuDropDown.classList.contains("h-56","opacity-100")){
-            menuDropDown.classList.remove("h-56","opacity-100");
-        }else{
-            menuDropDown.classList.add("h-56","opacity-100");
-        }
-    });
-
+  const toggle = document.querySelector(".menu-toggle");
+  const menuDropDown = document.querySelector(".menu-drob");
+  
+  toggle.addEventListener("click", () => {
+    menuDropDown.classList.toggle("show"); // Simpler toggle for visibility
+  });
     // check loction page you at right now
     const currentPage = window.location.pathname.split("/").pop().replace("%20", " ").replace("%20"," ");
     const allPages = document.querySelectorAll(".bg-color"); 
